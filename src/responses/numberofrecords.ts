35aedec01de87e905975afd13eb3879ea0a7632b
Myrotvorets <support@myrotvorets.center>
2021-12-09 16:50:24 +0200

    Configure Renovate

--- src/responses/numberofrecords.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from './response';
-
-// ans_type = 8
-export class NumberOfRecords extends Response {
-    public get numberOfRecords(): number {
-        return this._raw.data.fotos.length ? this._raw.data.fotos[0].par1 : -1;
-    }
-}
