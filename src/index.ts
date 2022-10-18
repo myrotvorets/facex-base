2a4af770e9fdb183c89e7c92329e75522f8cecac
Myrotvorets <support@myrotvorets.center>
2022-10-18 04:49:37 +0300

    Bug fix

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export * from './client';
 export * from './responses';
 export * from './responsefactory';
 export * from './svcclient';
+export * from './videoclient';
 export * from './exceptions';
 export * from './interfaces';
 export * from './guidgenerators';
