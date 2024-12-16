cd495c57e628eb02618e47850b53e1c19d9c6549
Myrotvorets <support@myrotvorets.center>
2024-12-16 03:28:41 +0200

    Update ESLint config

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,13 @@
-/** @type {import('ts-jest').InitialOptionsTsJest} */
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
     collectCoverage: process.env.COLLECT_COVERAGE !== '0',
-    collectCoverageFrom: [
-        'src/**/*.ts',
-    ],
+    collectCoverageFrom: ['src/**/*.ts'],
     clearMocks: true,
     verbose: true,
-    testPathIgnorePatterns: [
-        '<rootDir>/dist/',
-        '<rootDir>/node_modules/',
-    ],
-    reporters: [
-        "default",
-        process.env.GITHUB_ACTIONS === 'true' ? 'jest-github-actions-reporter' : null,
-    ].filter(Boolean),
-    testLocationInResults: true
+    testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+    reporters: ['default', process.env.GITHUB_ACTIONS === 'true' ? 'jest-github-actions-reporter' : null].filter(
+        Boolean,
+    ),
+    testLocationInResults: true,
 };
